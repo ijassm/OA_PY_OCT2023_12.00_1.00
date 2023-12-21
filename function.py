cedf43d4cc9c6c52e1524eeb3e6cf29cb2dc62d4
“ijassm” <ijass.ocean@gmail.com>
2023-12-21 13:12:39 +0530

    function types of arguments --21 DEC 2023

--- function.py
+++ function.py
@@ -21,22 +21,84 @@
 
 # greeting("ajay")
 
-l1 = []
+# l1 = []
 
 
-def createList(n):
-    for i in range(1, n + 1):
-        l1.append(i)
+# def createList(n):
+#     for i in range(1, n + 1):
+#         l1.append(i)
 
 
-createList(15)
-print(l1)
-l1.clear()
-print("end")
-
-createList(5)
-print(l1)
-l1.clear()
-print("end")
+# createList(15)
+# print(l1)
+# l1.clear()
+# print("end")
 
 # createList(5)
+# print(l1)
+# l1.clear()
+# print("end")
+
+# createList(5)
+
+
+# -------------------------------------------------------
+
+# Number of Arguments
+"""By default, a function must be called with
+the correct number of arguments. Meaning that
+if your function expects 2 arguments, you have to
+call the function with 2 arguments, not more, and not less."""
+
+
+# This function expects 2 arguments, and gets 2 arguments:
+
+
+# def my_function(fname, lname):
+#     print(fname + " " + lname)
+
+
+# my_function("Emil", "Refsnes")
+
+# Arbitrary Arguments, *args
+
+# def my_function(*names):
+#     print(names)
+#     print("The youngest child is " + names[2])
+
+
+# my_function("Emil", "Tobias", "Linus")
+
+
+# Keyword Arguments
+
+
+# def my_function(name1, name2, name3):
+#     print("The youngest child is " + name3)
+
+
+# my_function(name3="john", name1="bala", name2="xyz")
+
+
+# Arbitrary Keyword Arguments, **kwargs
+
+
+# def my_function(**person):
+#     print(person)
+#     print("His last name is " + person["lname"])
+
+
+# my_function(fname="Tobias", lname="Refsnes")
+
+
+# Default Parameter Value
+
+
+def my_function(country="Norway"):
+    print("I am from " + country)
+
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
---
+++ function_return.py
@@ -0,0 +1,50 @@
+# s = 0
+
+
+# def sum(a, b):
+#     global s
+#     s = a + b
+
+
+# sum(2, 4)
+# print(s)
+
+# ---------------------
+# s = 0
+# m = 0
+
+
+# def sum(a, b):
+#     global s
+#     s = a + b
+
+
+# def mul(a, b):
+#     global m
+#     m = a * b
+
+
+# sum(3, 5)
+# mul(4, 3)
+
+
+# t = s + m
+
+# print(t)
+
+# ---------------------------
+
+# def sum(a, b):
+#     return a + b
+
+
+# def mul(a, b):
+#     return a * b
+
+
+# t = sum(3, 5) + mul(4, 3)
+
+# print(t)
+# print(sum(2, 4))
+# print(sum(5, 3))
+# print(sum(4, 6))
